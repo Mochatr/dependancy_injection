1bd6bdea19f8af72a569499d7f428f30251a7bdc
Mochatr <mohamedchatr8@gmail.com>
2026-06-24 22:10:49 +0100

    Instantiation dynamique

--- src/main/java/net/mohamed/pres/pres2.java
+++ src/main/java/net/mohamed/pres/pres2.java
@@ -5,6 +5,7 @@ import net.mohamed.metier.IMetier;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class pres2 {
@@ -20,6 +21,9 @@ public class pres2 {
         String metierClassName = scanner.nextLine();
         Class cMetier = Class.forName(metierClassName);
         IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance(d);
+        // IMetier metier = (IMetier) cMetier.getConstructor().newInstance(d);
+        // Method setDao = cMetier.getDeclaredMethod("setDao", IDao.class);
+        // setDao.invoke(metier,d);
 
         System.out.println("RES"+metier.calcul());
     }
